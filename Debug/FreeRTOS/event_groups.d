7f6885fae0f2d704ecbfa407f2b38cf96ff4acdf
chihyu1206 <chihyu.chen1206@gmail.com>
2021-09-07 05:19:45 +0800

    Add new files

---
+++ Debug/FreeRTOS/event_groups.d
@@ -0,0 +1,40 @@
+FreeRTOS/event_groups.o: ../FreeRTOS/event_groups.c \
+ /home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/FreeRTOS.h \
+ /home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/FreeRTOSConfig.h \
+ /home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/projdefs.h \
+ /home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/portable.h \
+ /home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/deprecated_definitions.h \
+ /home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/ARM_CM4F/portmacro.h \
+ /home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/mpu_wrappers.h \
+ /home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/task.h \
+ /home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/list.h \
+ /home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/timers.h \
+ /home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/task.h \
+ /home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/event_groups.h \
+ /home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/timers.h
+
+/home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/FreeRTOS.h:
+
+/home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/FreeRTOSConfig.h:
+
+/home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/projdefs.h:
+
+/home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/portable.h:
+
+/home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/deprecated_definitions.h:
+
+/home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/ARM_CM4F/portmacro.h:
+
+/home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/mpu_wrappers.h:
+
+/home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/task.h:
+
+/home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/list.h:
+
+/home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/timers.h:
+
+/home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/task.h:
+
+/home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/event_groups.h:
+
+/home/chihyu/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091713/FreeRTOS/include/timers.h:
